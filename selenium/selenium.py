71b0ae7c09204cc2f351240d0871f541b9fb69fb
maulana <maulana20@versatech.co.id>
2019-03-15 09:46:03 +0700

    logger
    Signed-off-by: maulana <maulana20@versatech.co.id>

--- selenium/selenium.py
+++ selenium/selenium.py
@@ -3,6 +3,8 @@ from selenium.webdriver.common.keys import Keys
 
 import time
 
+import logging
+
 driver = webdriver.Chrome()
 driver.get("http://localhost/login/")
 assert "Login" in driver.title
@@ -11,5 +13,43 @@ elem = driver.find_element_by_xpath("//input[@placeholder='Password']").send_key
 elem = driver.find_element_by_xpath("//button[contains(text(), 'Login')]").click()
 
 time.sleep(10)
-assert "No results found." not in driver.page_source
+
+elem = driver.find_element_by_xpath("//span/a[@title='Collapse Menu']").click()
+time.sleep(5)
+
+elem = driver.find_element_by_xpath("//span/a[@title='Collapse Menu']").click()
+time.sleep(5)
+
+elem = driver.find_element_by_id("logout").click()
+
+time.sleep(5)
+
+#assert "No results found." not in driver.page_source
 driver.close()
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+#Create a file handler
+handler_warn = logging.FileHandler('warning_log.txt')
+handler_warn.setLevel(logging.WARNING)
+
+handler_info = logging.FileHandler('info_log.txt')
+handler_info.setLevel(logging.INFO)
+
+#create a logging format
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler_warn.setFormatter(formatter)
+handler_info.setFormatter(formatter)
+
+
+#add the handler to the logger
+
+logger.addHandler(handler_warn)
+logger.addHandler(handler_info)
+
+
+logger.info(elem)
+logger.warning(elem)
