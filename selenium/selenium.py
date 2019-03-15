84465994c9fbf5a9f4fb76cf07787b6cdcfe0be1
maulana <maulana20@versatech.co.id>
2019-03-15 09:09:08 +0700

    selenium with xpath
    Signed-off-by: maulana <maulana20@versatech.co.id>

--- selenium/selenium.py
+++ selenium/selenium.py
@@ -1,12 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+import time
+
 driver = webdriver.Chrome()
-driver.get("https://www.google.com/")
-assert "Google" in driver.title
-elem = driver.find_element_by_name("q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
+driver.get("http://localhost/login/")
+assert "Login" in driver.title
+elem = driver.find_element_by_xpath("//input[@placeholder='Username']").send_keys("maulana_versatech")
+elem = driver.find_element_by_xpath("//input[@placeholder='Password']").send_keys("Csatversa123")
+elem = driver.find_element_by_xpath("//button[contains(text(), 'Login')]").click()
+
+time.sleep(10)
 assert "No results found." not in driver.page_source
 driver.close()
